dabe11ba7ef7697d03fec7ab6f1b9fc1acfeab60
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-10-09 20:48:08 +0500

    Update

---
+++ app.js
@@ -0,0 +1,8 @@
+// Get the button element by its id
+var button = document.getElementById("myButton");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    // Function to execute when the button is clicked
+    console.log("Button clicked!");
+});
